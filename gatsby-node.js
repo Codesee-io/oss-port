04fd29ceb6b5de52136a6c33af79bedc1aa8ba9a
Joshua Leven <josh@codesee.io>
2021-09-01 16:23:33 -0700

    Calculate contributors and clean up other data: - Make sure PRs Created count unmerged PRs - Rename bot id to botId - Extract github api stuff into a function

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,92 +45,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     let githubData = {};
 
     if (githubAPI) {
-      try {
-        let thereMayBeMoreData = true;
-        const data = await githubAPI(`
-        query mergedPRs($owner: String!, $repo: String!) {
-          repository(owner: $owner, name: $repo) {
-            pullRequests(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}, states: MERGED) {
-              pageInfo {
-                startCursor
-                endCursor
-              }
-              totalCount
-              nodes {
-                createdAt
-                mergedAt
-                number
-                author {
-                  ... on Bot {
-                    id
-                  }
-                }
-              }
-            }
-          }
-          rateLimit {
-            cost,
-            remaining,
-            limit
-          }
-        }
-        `, {
-          owner: node.parent.relativeDirectory,
-          repo: node.parent.name,
-        });
-
-        const mergedThisMonth = data.repository.pullRequests.nodes.filter(({mergedAt, author}) => {
-          if (author?.id) {
-            return false;
-          }
-          const date = new Date(mergedAt);
-          const now = Date.now();
-          const isThisMonth = now - date < 30 /* days */ * 24 * 60 * 60 * 1000;
-          if (!isThisMonth) {
-            // This PR is from more than a month ago.
-            // Since the data is sorted (by updated_at),
-            // this PROBABLY means we have fetched all relevant data
-            thereMayBeMoreData = false; // most likely
-          }
-          
-          return isThisMonth;
-        });
-
-        githubData['prsMerged'] = {
-          count: mergedThisMonth.length,
-          maybeMore: thereMayBeMoreData
-        }
-
-        thereMayBeMoreData = true;
-
-        const createdThisMonth = data.repository.pullRequests.nodes.filter(({createdAt, author}) => {
-          // If author.id is set, this means that the author is a bot because specifically cast
-          // the author as a bot and get the id in the query.
-          if (author?.id) {
-            // TODO: Do we want to include bots?
-            return false;
-          }
-          const date = new Date(createdAt);
-          const now = Date.now();
-          const isThisMonth = now - date < 30 /* days */ * 24 * 60 * 60 * 1000;
-          if (!isThisMonth) {
-            // This PR is from more than a month ago.
-            // Since the data is sorted (by updated_at),
-            // this PROBABLY means we have fetched all relevant data
-            thereMayBeMoreData = false; // most likely
-          }
-          
-          return isThisMonth;
-        });
-
-        githubData['prsCreated'] = {
-          count: createdThisMonth.length,
-          maybeMore: thereMayBeMoreData
-        }
-
-      } catch (err) {
-        console.warn(err);
-      }
+      githubData = await calculateGithubData(
+        githubAPI, 
+        node.parent.relativeDirectory, 
+        node.parent.name
+      );
     }
 
     createPage({
@@ -146,26 +65,150 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 };
 
 
-/*
-{
-  repository(name: "shipment-tracker", owner: "distributeaid") {
-    pullRequests(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}, states: MERGED) {
-      pageInfo {
-        startCursor
-        endCursor
+
+async function calculateGithubData(githubAPI, owner, repo) {
+  let githubData = {};
+  try {
+    const mergedPRs = await githubAPI(`
+    query mergedPRs($owner: String!, $repo: String!) {
+      repository(owner: $owner, name: $repo) {
+        pullRequests(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}, states: MERGED) {
+          pageInfo {
+            startCursor
+            endCursor
+          }
+          totalCount
+          nodes {
+            createdAt
+            mergedAt
+            number
+            author {
+              ... on Bot {
+                botId: id
+              }
+              ... on User {
+                login
+              }
+            }
+    
+          }
+        }
       }
-      totalCount
-      nodes {
-        createdAt
-        mergedAt
-        number
+      rateLimit {
+        cost,
+        remaining,
+        limit
       }
     }
+    `, {
+      owner,
+      repo,
+    });
+
+
+    let mergedThisMonth = 0;
+    const contributorsThisMonth = new Set();
+    let thereMayBeMoreMergeData = true;
+
+    mergedPRs.repository.pullRequests.nodes.forEach(({mergedAt, author}) => {
+      // If author.botId is set, this means that the author is a bot because specifically cast
+      // the author as a bot and get the id in the query.
+      if (author?.botId) {
+        return false;
+      }
+      const date = new Date(mergedAt);
+      const now = Date.now();
+      const isThisMonth = now - date < 30 /* days */ * 24 * 60 * 60 * 1000;
+      if (isThisMonth) {
+        mergedThisMonth += 1;
+        if (author?.login) {
+          contributorsThisMonth.add(author.login);
+        }
+      } else {
+        // This PR is from more than a month ago.
+        // Since the data is sorted (by updated_at),
+        // this PROBABLY means we have fetched all relevant data
+        thereMayBeMoreMergeData = false; // most likely
+      }
+    });
+
+    githubData['prsMerged'] = {
+      count: mergedThisMonth,
+      maybeMore: thereMayBeMoreMergeData,
+    };
+
+    let thereMayBeMoreCreatedData = true;
+    const createdPRs = await githubAPI(`
+    query createdPRs($owner: String!, $repo: String!) {
+      repository(owner: $owner, name: $repo) {
+        pullRequests(first: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+          pageInfo {
+            startCursor
+            endCursor
+          }
+          totalCount
+          nodes {
+            createdAt
+            mergedAt
+            number
+            author {
+              ... on Bot {
+                botId: id
+              }
+              ... on User {
+                login
+              }
+            }
+          }
+        }
+      }
+      rateLimit {
+        cost
+        remaining
+        limit
+      }
+    }        
+    `, {
+      owner,
+      repo,
+    });
+
+    let createdThisMonth = 0;
+    createdPRs.repository.pullRequests.nodes.forEach(({createdAt, author}) => {
+      // If author.botId is set, this means that the author is a bot because specifically cast
+      // the author as a bot and get the id in the query.
+      if (author?.botId) {
+        // TODO: Do we want to include bots?
+        return false;
+      }
+      const date = new Date(createdAt);
+      const now = Date.now();
+      const isThisMonth = now - date < 30 /* days */ * 24 * 60 * 60 * 1000;
+      if (isThisMonth) {
+        createdThisMonth += 1;
+        if (author?.login) {
+          contributorsThisMonth.add(author.login);
+        }
+      } else {
+        // This PR is from more than a month ago.
+        // Since the data is sorted (by updated_at),
+        // this PROBABLY means we have fetched all relevant data
+        thereMayBeMoreCreatedData = false; // most likely
+      }
+    });
+
+    githubData['prsCreated'] = {
+      count: createdThisMonth,
+      maybeMore: thereMayBeMoreCreatedData
+    }
+    githubData['contributors'] = {
+      count: contributorsThisMonth.size,
+      maybeMore: thereMayBeMoreMergeData || thereMayBeMoreCreatedData,
+    }
+
+  } catch (err) {
+    console.warn(err);
   }
-  rateLimit {
-    cost,
-    remaining,
-    limit
-  }
+
+  return githubData;
 }
- */
