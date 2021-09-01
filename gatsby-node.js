8f67c1bb07e074b5b93fa4c51d27e3f056615e9f
Joshua Leven <josh@codesee.io>
2021-09-01 13:34:06 -0700

    We can get data from github

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const { graphql: github } = require("@octokit/graphql");
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
@@ -26,15 +27,116 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     return;
   }
 
+  let githubAPI;
+  if (process.env.GITHUB_PERSONAL_ACCESS_TOKEN) {
+    githubAPI = github.defaults({
+      headers: {
+        authorization: `token ${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });  
+  }
+
   // Create a page for each project
-  projects.data.allMdx.nodes.forEach((node) => {
+  const nodes = projects.data.allMdx.nodes;
+  for (const node of nodes) {
+    let githubData = {};
+
+    if (githubAPI) {
+      try {
+        let thereMayBeMoreData = true;
+        const data = await githubAPI(`
+        query mergedPRs($owner: String!, $repo: String!) {
+          repository(owner: $owner, name: $repo) {
+            pullRequests(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}, states: MERGED) {
+              pageInfo {
+                startCursor
+                endCursor
+              }
+              totalCount
+              nodes {
+                createdAt
+                mergedAt
+                number
+                author {
+                  ... on Bot {
+                    id
+                  }
+                }
+              }
+            }
+          }
+          rateLimit {
+            cost,
+            remaining,
+            limit
+          }
+        }
+        `, {
+          owner: node.parent.relativeDirectory,
+          repo: node.parent.name,
+        });
+
+        const mergedThisMonth = data.repository.pullRequests.nodes.filter(({mergedAt, author}) => {
+          if (author?.id) {
+            return false;
+          }
+          const date = new Date(mergedAt);
+          const now = Date.now();
+          const isThisMonth = now - date < 30 /* days */ * 24 * 60 * 60 * 1000;
+          if (!isThisMonth) {
+            // This PR is from more than a month ago.
+            // Since the data is sorted (by updated_at),
+            // this PROBABLY means we have fetched all relevant data
+            thereMayBeMoreData = false; // most likely
+          }
+          
+          return isThisMonth;
+        });
+
+        githubData = {
+          prsMerged: {
+            count: mergedThisMonth.length,
+            maybeMore: thereMayBeMoreData
+          }
+        }
+      } catch (err) {
+        console.warn(err);
+      }
+    }
+
     createPage({
       path: node.slug,
       component: projectTemplate,
       context: {
         id: node.id,
         slug: node.slug,
+        githubData,
       },
     });
-  });
+  }
 };
+
+
+/*
+{
+  repository(name: "shipment-tracker", owner: "distributeaid") {
+    pullRequests(first: 100, orderBy: {field: UPDATED_AT, direction: DESC}, states: MERGED) {
+      pageInfo {
+        startCursor
+        endCursor
+      }
+      totalCount
+      nodes {
+        createdAt
+        mergedAt
+        number
+      }
+    }
+  }
+  rateLimit {
+    cost,
+    remaining,
+    limit
+  }
+}
+ */
