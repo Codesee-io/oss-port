6922aadde9bc42fc6387be7d5fec0b9c65ae4e24
Maxime Preaux <hello@maxime.dev>
2021-09-01 17:42:22 -0700

    Round metrics

--- src/components/RepoStats.tsx
+++ src/components/RepoStats.tsx
@@ -1,21 +1,28 @@
 import React, { FunctionComponent } from "react";
 
+type Metric = {
+  count: number;
+  maybeMore: boolean;
+};
+
 type Props = {
   stats: {
-    prsCreated: {
-      count: number;
-      maybeMore: boolean;
-    };
-    prsMerged: {
-      count: number;
-      maybeMore: boolean;
-    };
-    contributors: {
-      count: number;
-      maybeMore: boolean;
-    };
+    prsCreated: Metric;
+    prsMerged: Metric;
+    contributors: Metric;
   };
 };
+
+function formatMetric(metric: Metric) {
+  if (metric.maybeMore) {
+    if (metric.count > 20) {
+      return Math.floor(metric.count / 10) * 10 + "+";
+    } else {
+      return metric.count + "+";
+    }
+  }
+  return metric.count;
+}
 
 const RepoStats: FunctionComponent<Props> = ({ stats }) => {
   return (
@@ -23,19 +30,25 @@ const RepoStats: FunctionComponent<Props> = ({ stats }) => {
       <h2 className="text-sm font-bold text-black-500 mb-4">Last 30 days</h2>
       <div className="flex justify-between text-black-500 space-x-4 text-center">
         <div>
-          <div className="text-3xl font-bold">{stats.prsCreated.count}</div>
+          <div className="text-3xl font-bold">
+            {formatMetric(stats.prsCreated)}
+          </div>
           <small className="text-xs uppercase whitespace-nowrap">
             PRs opened
           </small>
         </div>
         <div>
-          <div className="text-3xl font-bold">{stats.prsMerged.count}</div>
+          <div className="text-3xl font-bold">
+            {formatMetric(stats.prsMerged)}
+          </div>
           <small className="text-xs uppercase whitespace-nowrap">
             PRs merged
           </small>
         </div>
         <div>
-          <div className="text-3xl font-bold">{stats.contributors.count}</div>
+          <div className="text-3xl font-bold">
+            {formatMetric(stats.contributors)}
+          </div>
           <small className="text-xs uppercase whitespace-nowrap">
             Contributors
           </small>
