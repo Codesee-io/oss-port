ea789f2e0676d41b5ec4262cb9ebec1d4997a80e
Shanea Leven <shanea@codesee.io>
2021-09-18 16:20:26 -0700

    table styling

--- src/pages/swag.tsx
+++ src/pages/swag.tsx
@@ -9,12 +9,60 @@ import CallToAction from "../components/CallToAction";
 const pageStyles = {
     color: "#232129",
     padding: "32px 96px 96px 96px",
-    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+    fontFamily: "SF Pro Text, -apple-system, Roboto, sans-serif, serif",
 }
 
 const linkStyles = {
     color: "rgb(0, 84, 149)",
     textDecoration: "underline"
+}
+const tableStylesWhiteHeader = {
+    backgroundColor: "#ffffff",
+    padding: "17px 22px 22px 17px",
+    fontSize: "21px",
+    fontWeight: 500,
+    color: "#021443",
+    textAlign: "center" as "center",
+
+}
+const tableStylesLabel = {
+    padding: "17px 22px 22px 17px",
+    fontSize: "14px",
+    fontWeight: 500,
+    color: "#021443",
+    textAlign: "right" as "right",
+    textTransform: "uppercase" as "uppercase",
+
+}
+
+const tableStylesTeal = {
+    backgroundColor: "#E9F7F680",
+    padding: "17px 22px 22px 17px",
+    fontSize: "14px",
+    color: "#021443",
+    lineHeight: "19px",
+    height: "72px",
+    width: "335px",
+    textAlign: "center" as "center",
+}
+
+const tableStylesWhite = {
+    backgroundColor: "#ffffff80",
+    padding: "17px 22px 22px 17px",
+    fontSize: "14px",
+    color: "#021443",
+    textAlign: "center" as "center",
+
+}
+
+const tableStylesNote = {
+    marginTop: "40px",
+    padding: "17px 22px 22px 17px",
+    fontSize: "14px",
+    color: "#021443",
+    width: "335px",
+    textAlign: "center" as "center",
+
 }
 
 // markup
@@ -36,28 +84,27 @@ const Swag = () => {
                         Claim your swag
                     </CallToAction>
 
-                    <div className="grid gap-4 grid-cols-3">
+                    <div className="grid gap-y-0 gap-x-4 grid-cols-3 pt-12">
                         <div></div>
-                        <div>Maintainers</div>
-                        <div>Contributors</div>
-                        <div>Crew sweatshirt</div>
-                        <div>Use CodeSee Maps and publish a map to your OSS Port project</div>
-                        <div>Submit at least 3 PRs to 2 OSS Port projects</div>
-                        <div>Crew t-shirt</div>
-                        <div>Submit a project to the OSS Port community</div>
-                        <div>Submit at least 5 PRs to an OSS Port project</div>
-                        <div>vinyl sticker</div>
-                        <div>Included—we got you</div>
-                        <div>Submit your first PR to an OSS Port project</div>
-                    </div>
-
-                    <div style={{ maxWidth: "450px" }}>
-                        <p className="pt-20 pb-4">
+                        <div style={tableStylesWhiteHeader}>Maintainers</div>
+                        <div style={tableStylesWhiteHeader}>Contributors</div>
+                        <div style={tableStylesLabel}>Crew sweatshirt</div>
+                        <div style={tableStylesTeal}>Use CodeSee Maps and publish a map to your OSS Port project</div>
+                        <div style={tableStylesTeal}>Submit at least 3 PRs to 2 OSS Port projects</div>
+                        <div style={tableStylesLabel}>Crew t-shirt</div>
+                        <div style={tableStylesWhite}>Submit a project to the OSS Port community</div>
+                        <div style={tableStylesWhite}>Submit at least 5 PRs to an OSS Port project</div>
+                        <div style={tableStylesLabel}>vinyl sticker</div>
+                        <div style={tableStylesTeal}>Included—we got you</div>
+                        <div style={tableStylesTeal}>Submit your first PR to an OSS Port project</div>
+                        <div></div>
+                        <div></div>
+                        <div style={tableStylesNote}>
                             Only GitHub PRs merged to OSS Port projects during Hacktoberfest 2021 are eligible for rewards.
-                        </p>
+                        </div>
                     </div>
 
-                    
+
 
                 </div>
 
