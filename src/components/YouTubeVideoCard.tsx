53eceadf373012fea24d4def0164133c21e0f44a
Ramón Huidobro <hola@ramonh.dev>
2021-10-21 16:14:23 +0200

    Add YouTubeVideoCard component

---
+++ src/components/YouTubeVideoCard.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+
+interface Props {
+   youTubeID: string,
+   title: string,
+   body: string
+}
+
+const YouTubeVideoCard: FunctionComponent<Props> = ({ youTubeID, title, body }) => {
+  const videoLink = `https://youtube.com/watch?v=${youTubeID}`;
+  const imageLink = `https://img.youtube.com/vi/${youTubeID}/mqdefault.jpg`;
+  const alt = `Video thumbnail for ${youTubeID}`;
+  return (
+    <a href={videoLink} className="flex bg-white py-4 px-4 mb-4" rel="noopener" target="_blank">
+      <img src={imageLink} alt={alt} className="mr-8" />
+      <div className="flex flex-col justify-center text-black-500">
+        <h3 className="font-semibold text-lg mb-4">{title}</h3>
+        <p>{body}</p>
+      </div>
+    </a>
+  );
+};
+
+export default YouTubeVideoCard;
