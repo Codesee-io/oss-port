0e766cbc0b51aa094f6e23d1f0d8fb1ab0ce4814
Maxime Preaux <hello@maxime.dev>
2022-05-06 14:48:28 -0700

    Links in the footer

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -4,3 +4,4 @@ export const HOW_TO_LIST_PROJECT_URL =
 export const DISCORD_LINK = "https://discord.gg/JbAChX3a3a";
 
 export const RESOURCES_LINK = "/resources/";
+export const REPO_URL = "https://github.com/Codesee-io/oss-port";
