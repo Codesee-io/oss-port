585f7fa6c9e525973edfa6976820ae74346f5b54
Maxime Preaux <hello@maxime.dev>
2022-05-09 07:58:52 -0700

    Added a banner

--- app/components/RootLayout.tsx
+++ app/components/RootLayout.tsx
@@ -5,6 +5,9 @@ import Header from "./Header";
 const RootLayout: FC = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
+      <div className="p-3 text-white bg-aqua-700 text-center">
+        OSS Port is now Open-Source Hub!
+      </div>
       <Header />
       <main className="flex-grow">{children}</main>
       <Footer />
